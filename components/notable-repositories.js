6cd39649bc97ebf7de1ff5831c7c07f377c3c9b8
vicskips <vicskips@gmail.com>
2020-05-17 22:13:53 +0100

    Changed font size

--- components/notable-repositories.js
+++ components/notable-repositories.js
@@ -3,7 +3,7 @@ import PostPreview from "./post-preview";
 export default function NotableRepositories({ posts }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+      <h2 className="mb-8 text-5xl md:text-5xl font-bold tracking-tighter leading-tight">
         Notable repositories
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
