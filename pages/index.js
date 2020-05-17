494a67b7032863686146fe297f9d032c4ed90ac3
vicskips <vicskips@gmail.com>
2020-05-17 22:14:29 +0100

    Added bio to homepage

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,50 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
+          <h2 className="mb-8 text-5xl md:text-5xl font-bold tracking-tighter leading-tight">
+            Hi, I'm Hen 👋
+          </h2>
+          <p>
+            Currently, I am finishing my final year in college where I am
+            learning,{" "}
+          </p>
+          <ul className="list-disc list-inside mt-3 ml-3 mb-8">
+            <li>C#</li>
+            <li>HTML</li>
+            <li>SQL</li>
+            <li>Cisco Packet Tracer</li>
+          </ul>
+
+          <p>
+            Outside of college I am working as a part time chef and have been
+            self learning,
+          </p>
+          <ul className="list-disc list-inside mt-3 ml-3 mb-8">
+            <li>NodeJS</li>
+            <li>Express</li>
+            <li>Cypress</li>
+            <li>NextJS</li>
+          </ul>
+
+          <p>Hear are a couple places you can find me online</p>
+
+          <ul className="list-disc list-inside mt-3 ml-3 mb-8">
+            <li>
+              <a href="https://twitter.com/w_crankshaw" target="__blank">
+                <strong>twitter</strong>.com/w_crankshaw
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/Vicskips" target="__blank">
+                <strong>github</strong>.com/Vicskips
+              </a>
+            </li>
+            <li>
+              <a href="mailto:wjjcrankshaw@gmail.com">
+                wjjcrankshaw@<strong>gmail</strong>.com
+              </a>
+            </li>
+          </ul>
 
           {allPosts.length > 0 && <NotableRepositories posts={allPosts} />}
         </Container>
